f1c28561370fb41c9d2ecefb681f4a7bb225b1f5
Carlos de la Torre <48769508+c-torre@users.noreply.github.com>
2020-07-15 20:14:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,336 +1,265 @@
-"""
-<[Re] Recanatesi (2015). Neural Network Model of Memory Retrieval>
-Copyright (C) <2019>  <de la Torre-Ortiz C, Nioche A>
-
-This program is free software: you can redistribute it andtimeor modify
-it unt_stepe terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
+# %%
 import os
-import pickle
-from multiprocessing import cpu_count
-from multiprocessing.pool import Pool
+import pickle  # to be replaces with .npy files
 
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import matplotlib.style as mpstyle
 import numpy as np
-from tqdm import tqdm
 
-import tools.plots as plot
-import tools.recall_performance
-import tools.sine_wave
-
-FIG_DIR = "fig"
-
-os.makedirs(FIG_DIR, exist_ok=True)
-
-np.seterr(all="raise")
+RESULTS_DIR = "results"
+# To use when refactoring without pickle dictionary
+# ACTIVITIES_DIR = os.path.join(RESULTS_DIR, "activities")
+# SIMILARITIES_DIR = os.path.join(RESULTS_DIR, "similarities")
 
 
-# === Parameters ===
-# Architecture
-NUM_NEURONS = 10 ** 5
-NUM_MEMORIES = 16
+try:
+    job_env = int(os.getenv("SLURM_ARRAY_TASK_ID")) # Changes seed per cluster simulation
+except:
+    job_env = 0 # Default seed for non-cluster use
+np.random.seed(job_env)
+
+mpstyle.use("fast")  # Faster plots for prototyping
+
+NUM_NEURONS = 10 ** 5  # "N"
+NUM_MEMORIES = 16  # "P"
 # Activation
-T_DECAY = 0.01
-# Gain
-GAIN_THRESHOLD = 0
-GAIN_EXP = 2 / 5
-# Hebbian rule
-EXCITATION = 13_000
-SPARSITY = 0.1
-# Inhibition
-SIN_MIN = 0.7
-SIN_MAX = 1.06
-T_OSCILLATION = 1
-PHASE_SHIFT = 0.75
-# Short term association
-CONT_FORTH = 1500
-CONT_BACK = 400
+T_DECAY = 0.01  # "tau"
 # Time
-T_CYCLES = 8.0  # 14
-T_STEP = 0.001
+T_STEP = 0.001  # "dt"
+T_TOT = 450 # "T"; Half time iterations for prototyping (default: 450)
+T_SIMULATED = int(T_TOT // T_STEP)
+# Hebbian rule
+EXCITATION = 12_500  # "J1"
+SPARSITY = 0.1  # "f0"
+# Gain
+GAIN_THRESHOLD = 0  # set as *(x > 0) in gain function in matlab
+GAIN_EXP = 1 / 3  # set in gain function in matlab
+# Inhibition
+SIN_MIN = 0.4 * EXCITATION  # "J_0m"
+SIN_MAX = 1.2 * EXCITATION  # "J_0M"
 # Noise
-NOISE_VAR = 65
-# Initialization
-INIT_RATE = 1
-FIRST_MEMORY = 7
-# Recall
-RECALL_THRESHOLD = 15
-# Replication parameters
-PARAM_NOISE = 700
-# 10 we see saw peaks and bubbles and good transitions
-# print(param_noise)
-PARAM_INHIBITION = 1
-# print(param_inhibition)
-PARAM_CURRENT = 1  # 4.75
+NOISE_STD = 65  # "noise_amp"
+# Forward and backward contiguity
+CONT_FORTH = 1500 / NUM_NEURONS  # "J_f"
+CONT_BACK = 850 / NUM_NEURONS  # "J_b"
+# R
+NUM_POINTS = T_TOT * 100  # ?? from matlab script
 
-# ==================
+def check_state(state):
+    STATES = ["SEED", "CONT_FORTH", "NOISE"]
+    assert state in STATES
 
+state = "CONT_FORTH"
+check_state(state)
 
-def inside_loop_connectivity_matrices(arg):
-    """ Compute the loop inside connectivities to parallelize """
+cont_forth = seed / NUM_POINTS
+cont_back = seed / NUM_POINTS
 
-    i, num_pops, pop, backward_cont, forward_cont = arg
-
-    regular = np.zeros(num_pops)
-    forward = np.zeros(num_pops)
-    backward = np.zeros(num_pops)
-
-    for j in range(num_pops):
-        regular[j] = np.sum((pop[i, :] - SPARSITY) * (pop[j, :] - SPARSITY))
-
-        forward[j] = np.sum(pop[i, forward_cont] * pop[j, forward_cont + 1])
-
-        backward[j] = np.sum(pop[i, backward_cont] * pop[j, backward_cont - 1])
-
-    return regular, forward, backward
+# #%%
+def build_logical(prev_logical):
+    composite = np.hstack((prev_logical, prev_logical))
+    try:
+        half_size = prev_logical.shape[1]
+    except:
+        half_size = 1
+    bottom = np.hstack((np.zeros(half_size), np.ones(half_size)))
+    composite = np.vstack((composite, bottom))
+    return composite
 
 
-def compute_connectivity_matrices(num_pops, pop, forward_cont, backward_cont):
-    """ Compute matrices with parallelization """
+def get_log_int_sizes():
+    population_sizes = np.array([NUM_NEURONS])
+    log_int = np.array(0)
 
-    regular_connectivity = np.zeros((num_pops, num_pops))
-    forward_connectivity = np.zeros((num_pops, num_pops))
-    backward_connectivity = np.zeros((num_pops, num_pops))
-
-    args = [(i, num_pops, pop, backward_cont, forward_cont) for i in range(num_pops)]
-
-    with Pool(cpu_count()) as pool:
-        pools = list(
-            tqdm(pool.imap(inside_loop_connectivity_matrices, args), total=len(args))
+    for i in range(NUM_MEMORIES):
+        new_sizes = np.array(
+            [
+                np.random.binomial(population_sizes[x], SPARSITY)
+                for x in np.arange(population_sizes.shape[0])
+            ]
         )
-        pool.close()
-        pool.join()
+        population_sizes = np.hstack(((population_sizes - new_sizes), new_sizes))
+        log_int = build_logical(log_int)
 
-    for i in range(num_pops):
-        regular, forward, backward = pools[i]
-        regular_connectivity[i, :] = regular
-        forward_connectivity[i, :] = forward
-        backward_connectivity[i, :] = backward
+    log_int = log_int[:, population_sizes > 0]
+    population_sizes = population_sizes[population_sizes > 0]
 
-    return regular_connectivity, forward_connectivity, backward_connectivity
+    return population_sizes, log_int
 
 
-def main():
-    """ Licensing and main execution """
-
-    print(
-        "<[Re] Recanatesi (2015)>  Copyright (C) <2019>\n"
-        "<de la Torre-Ortiz C, Nioche A>\n"
-        "This program comes with ABSOLUTELY NO WARRANTY.\n"
-        "This is free software, and you are welcome to redistribute it\n"
-        "under certain conditions; see the 'LICENSE' file for details.\n"
-    )
-
-    np.random.seed(123)
-
-    # Compute memory patterns
-    print("Computing memory patterns and neuron populations...")
-    memory_patterns = np.random.choice(
-        [0, 1], p=[1 - SPARSITY, SPARSITY], size=(NUM_NEURONS, NUM_MEMORIES)
-    )
-
-    # Compute populations
-    pop, neurons_per_pop = np.unique(
-        [tuple(i) for i in memory_patterns], axis=0, return_counts=True
-    )
-
-    num_pops = len(pop)
-
-    neurons_encoding_mem = [
-        (pop[:, mu] == 1).nonzero()[0] for mu in range(NUM_MEMORIES)
-    ]
-
-    # === Other pre-computations ===
-    num_iter = int(T_CYCLES / T_STEP)
-    # relative_excitation = excitation / NUM_NEURONS
-    time_param = T_STEP / T_DECAY
-
-    # Inhibition
-    sine_wave = np.zeros(num_iter)
-
-    # Connectivity
-    forward_cont = np.arange(NUM_MEMORIES - 1)
-    backward_cont = np.arange(1, NUM_MEMORIES)
-
-    # Noise
-    noise = np.zeros((num_pops, num_iter))
-
-    # Neuron dynamics
-    firing_rates = np.zeros([num_pops])
-    current = np.zeros([num_pops])
-    average_firing_rates_per_memory = np.zeros((NUM_MEMORIES, num_iter))
-    currents = np.zeros((num_pops, num_iter))
-    currents_memory = np.zeros((NUM_MEMORIES, num_iter))
-    # ==============================
-
-    # Compute sine wave
-    print("Calculating inhibition values...")
-
-    for time in range(num_iter):
-        sine_wave[time] = tools.sine_wave.sinusoid(
-            min_=SIN_MIN,
-            max_=SIN_MAX,
-            period=T_OSCILLATION,
-            time=time,
-            phase_shift=PHASE_SHIFT * T_OSCILLATION,
-            t_step=T_STEP,
+def get_connectivities(log_int):
+    connectivity_reg = log_int.T
+    # connectivity_reg = (log_int)[1:].T
+    connectivity_back = np.hstack(
+        (
+            np.zeros(connectivity_reg.shape[0])[:, None],
+            connectivity_reg[:, : NUM_MEMORIES - 1],
         )
+    )
+    connectivity_forth = np.hstack(
+        (connectivity_reg[:, 1:], np.zeros(connectivity_reg.shape[0])[:, None])
+    )
+    return connectivity_reg, connectivity_back, connectivity_forth
 
-    inhibition = -sine_wave * PARAM_INHIBITION
 
-    # Compute weights
-    print("Computing regular, forward and backward connectivity...")
+def prepare_times():
+    it_rec = np.linspace(1, T_SIMULATED, NUM_POINTS).astype(int)
+    time = np.arange(start=0, stop=T_TOT + T_STEP, step=T_STEP)
+    t_rates = time[it_rec - 1] - 1
+    return it_rec, time, t_rates
 
-    bkp_file = "bkp/connectivities.p"
 
-    os.makedirs(os.path.dirname(bkp_file), exist_ok=True)
-
-    if not os.path.exists(bkp_file):
-        connectivities = compute_connectivity_matrices(
-            num_pops, pop, forward_cont, backward_cont
-        )
-        pickle.dump(connectivities, open(bkp_file, "wb"))
-
-    else:
-        print("Loading connectivity from pickle file...")
-        connectivities = pickle.load(open(bkp_file, "rb"))
-
-    # regular_connectivity, forward_connectivity, backward_connectivity = \
-    #     compute_connectivity_matrices(num_pops, pop, forward_cont,
-    #                                   backward_cont)
-    regular_connectivity, forward_connectivity, backward_connectivity = connectivities
-
-    regular_connectivity *= EXCITATION  # MOD REMOVED / NUM_NEURONS
-    forward_connectivity *= CONT_FORTH
-    backward_connectivity *= CONT_BACK
-    inhibition *= EXCITATION  # MOD REMOVED / NUM_NEURONS
-
-    weights_without_inhibition = (
-        regular_connectivity + forward_connectivity + backward_connectivity
+def osc(t):
+    return (SIN_MIN + SIN_MAX) / 2 + (SIN_MIN - SIN_MAX) / 2 * np.sin(
+        2 * np.pi * t + np.pi / 2
     )
 
-    # Compute noise
-    print("Calculating uncorrelated Gaussian noise...")
 
-    for pop in range(num_pops):
-        noise[pop] = (
-            np.random.normal(
-                loc=0, scale=(NOISE_VAR * neurons_per_pop[pop]) ** 0.5, size=num_iter
-            )
-            / neurons_per_pop[pop]
-            * PARAM_NOISE
-        )  # MOD ADDED BACK
-
-    # Initialize firing rates
-    firing_rates[neurons_encoding_mem[FIRST_MEMORY]] = INIT_RATE
-
-    # Initialize current
-    c_ini = INIT_RATE ** (1 / GAIN_EXP) - GAIN_THRESHOLD
-    current[neurons_encoding_mem[FIRST_MEMORY]] = c_ini
-
-    bkp_file = "bkp/average_firing_rates_per_memory.p"
-
-    os.makedirs(os.path.dirname(bkp_file), exist_ok=True)
-
-    if not os.path.exists(bkp_file):
-        # Compute neuron dynamics
-        print("Computing dynamics at each time step...")
-
-        for time in tqdm(range(num_iter)):
-
-            # Update current
-            for pop in range(num_pops):
-                # Compute weights
-                weights = (
-                    weights_without_inhibition[pop, :] + inhibition[time]
-                )  # / NUM_NEURONS
-
-                # Compute input     CHANGE neur to neurons_per_pop[:]
-                s_v = neurons_per_pop[:] / NUM_NEURONS  # DID NOT CHANGE
-                input_v = np.sum(weights[:] * s_v * firing_rates[:])
-
-                current[pop] += time_param * (
-                    -current[pop] + input_v + noise[pop, time]
-                )
-
-                # Backup for the plot
-                currents[pop, time] = current[pop]
-
-            # Update firing rates
-            firing_rates[:] = 0
-            cond = (current + GAIN_THRESHOLD) > 0
-            firing_rates[cond] = (
-                current[cond] * PARAM_CURRENT + GAIN_THRESHOLD
-            ) ** GAIN_EXP
-
-            for memory_idx in range(NUM_MEMORIES):
-                f_r = firing_rates[neurons_encoding_mem[memory_idx]]
-                n_corresponding = neurons_per_pop[neurons_encoding_mem[memory_idx]]
-
-                average_firing_rates_per_memory[memory_idx, time] = np.average(
-                    f_r, weights=n_corresponding
-                )
-
-                c_mu = current[neurons_encoding_mem[memory_idx]]
-
-                currents_memory[memory_idx, time] = np.average(
-                    c_mu, weights=n_corresponding
-                )
-
-        pickle.dump(average_firing_rates_per_memory, open(bkp_file, "wb"))
-
-    else:
-        print("Loading connectivity from pickle file...")
-        average_firing_rates_per_memory = pickle.load(open(bkp_file, "rb"))
-
-    # Recall performance
-    counts_memory_recalls = tools.recall_performance.count_memory_recalls(
-        average_firing_rates_per_memory, RECALL_THRESHOLD
-    )
-    probability_recall_memories = tools.recall_performance.get_probability_recall(
-        counts_memory_recalls, T_CYCLES
+def noise(noise_std, population_sizes):
+    return (
+        noise_std
+        / np.sqrt(population_sizes)
+        * np.random.randn(population_sizes.shape[0])
+        / np.sqrt(T_STEP)
     )
 
-    # Plots
-    print("Plotting...")
-    # plot.currents(currents, dt=t_step,
-    #               type_="population", fig_num=0)
-    # plot.currents(currents_memory, dt=t_step,
-    #               type_="memory", fig_num=1)
 
-    # plot.firing_rates(average_firing_rates_per_memory, t_step=T_STEP)
-    # plot.attractors(average_firing_rates_per_memory, t_step=T_STEP)
+def gain(currents_vector):
+    # return (np.heaviside(currents_vector, 0) * currents_vector) ** 0.4
+    # def func(x):
+    # print("GAIN")
+    # currents_vector += np.random.choice([-1, 1]) * 10 ** -8  # Division by 0
+    # print(currents_vector)
+    adaption = np.heaviside(currents_vector, 0)
+    # print(adaption)
+    currents_vector *= adaption
+    #gains = np.exp(-1 / currents_vector ** 2) * (currents_vector ** GAIN_EXP)
+    gains = currents_vector ** GAIN_EXP
 
-    # plot.sine_wave(sine_wave, dt=t_step)
-    # plot.inhibition(inhibition, dt=t_step)
-
-    # plot.weights(weights_without_inhibition,
-    #              type_="no_inhibition", fig_num=0)
-    # plot.weights(regular_connectivity,
-    #              type_="regular", fig_num=1)
-    # plot.weights(forward_connectivity,
-    #              type_="forward", fig_num=2)
-    # plot.weights(backward_connectivity,
-    #              type_="backward", fig_num=3)
-
-    # plot.noise(noise, dt=t_step, t_tot=t_tot)
-
-    # plot.probability_recall_given_size(
-    #     neurons_encoding_mem, probability_recall_memories
+    # gains *= (np.exp(-1 / currents_vector ** 2) * (currents_vector ** GAIN_EXP)).astype(
+    #     float
     # )
+    # print(gains)
+    # print("GAIN")
+    return gains
+    # return (np.heavisi
 
-    print("Done!")
+
+def get_initial_currents(num_memories, connectivity_reg):
+    rnd_memory = np.ceil(np.random.random(10) * num_memories) - 1
+    # rnd_memory = np.random.randint(1, num_memories)
+    currents_init = connectivity_reg[:, rnd_memory]
+    # currents_init += connectivity_reg[:, 0]
+    return currents_init
 
 
-if __name__ == "__main__":
-    main()
+def mult_W(Sgs, Vs, connectivity_reg, connectivity_back, connectivity_forth):
+    sparsity_vect = np.ones(NUM_MEMORIES) * SPARSITY
+    result = (
+        connectivity_reg @ Vs  # (65536,) @ (16,) = (65536,)
+        - sparsity_vect @ Vs  # (16,) @ (16,) = (1,)
+        - connectivity_reg
+        @ sparsity_vect
+        * np.sum(Sgs)  # (65536, 16) @ (16,) * (1,) = (65536,)
+        + sparsity_vect @ sparsity_vect * np.sum(Sgs)  # (16,) @ (16,) * (1,) = (1,)
+        + CONT_FORTH * connectivity_forth @ Vs  # (1,) * (65536, 16) @ (16,) = (65536,)
+        + CONT_BACK * connectivity_back @ Vs  # (1,) * (65536, 16) @ (16,) = (65536,)
+    )
+    return result
+
+
+#%
+def evolv(
+    t, curr, population_sizes, connectivity_reg, connectivity_back, connectivity_forth,
+):
+    act = gain(curr)
+    sized_activations = population_sizes * act
+    Vs = connectivity_reg.T @ sized_activations
+    mult_W_ = mult_W(
+        sized_activations, Vs, connectivity_reg, connectivity_back, connectivity_forth,
+    )
+    noise_ = NOISE_STD / np.sqrt(population_sizes) *np.random.randn(population_sizes.shape[0]) / np.sqrt(T_STEP)
+    # noise_ = noise(NOISE_STD, population_sizes)
+    sine = osc(t)
+    result = (1 / T_DECAY * (-1 * curr + 1 / NUM_NEURONS * (EXCITATION * mult_W_ - sine * np.sum(population_sizes * act)) + noise_))
+    # print(result)
+    return result
+
+
+
+#%% THE SCRIPT STARTS HERE
+N = NUM_NEURONS
+P = NUM_MEMORIES
+f = SPARSITY
+Xi = (np.random.random((P,N)) < f).T
+
+V = np.array([[int(i) for i in bin(num)[2:].zfill(P)] for num in range(2**P)])
+powers_of_two = np.array([2 ** i for i in range(P-1,-1,-1)])
+v_n = Xi @ powers_of_two
+S = np.histogram(v_n, np.arange(-1,2**P)+0.5)[0]
+V = V[S>0,:]
+S = S[S>0]
+
+population_sizes_ = S
+log_int_ = V.T
+# population_sizes_, log_int_ = get_log_int_sizes()
+
+num_pops = population_sizes_.shape[0]
+connectivity_reg_, connectivity_back_, connectivity_forth_ = get_connectivities(log_int_)
+it_rec, time, t_rates = prepare_times()
+rates = np.zeros((num_pops, len(it_rec)))
+ind_t = 0
+
+rnd_memory = (np.ceil(np.random.random(1) * P) - 1).astype(int)
+currents = np.squeeze(connectivity_reg_[:, rnd_memory].astype(np.float))
+
+sparsity_vect = np.ones(NUM_MEMORIES) * SPARSITY
+rates = np.zeros((num_pops, len(time)))
+for it in range(len(time)):
+    t = time[it]
+    act = gain(currents.copy())
+    sized_activations = population_sizes_ * act
+    r_tot = np.sum(sized_activations)
+    noise_ = NOISE_STD / np.sqrt(population_sizes_) * norm.ppf(np.random.rand(population_sizes_.shape[0]))
+    Vs = sized_activations @ connectivity_reg_
+    mult_W_ =  connectivity_reg_ @ Vs- sparsity_vect @ Vs- connectivity_reg_ @ sparsity_vect * r_tot + sparsity_vect @ sparsity_vect * r_tot
+    contiguity_ = CONT_FORTH * connectivity_forth_ @ Vs + CONT_BACK * connectivity_back_ @ Vs
+    sine = osc(t) / N
+    evolv_ = T_STEP / T_DECAY * (-currents + EXCITATION / NUM_NEURONS * (mult_W_ + contiguity_) - sine * r_tot + noise_ / np.sqrt(T_STEP))
+    currents += evolv_
+    rates[:, it] = act
+
+#%
+
+# currents, rates, connectivity_reg_, population_sizes_ = main()
+
+# pickle.dump(rates, open(os.path.join(".", "rates.p"), "wb"))
+# rates = pickle.load(open(os.path.join(".", "rates.p"), "rb"))
+
+######### Most plotting stuff commented out for cluster
+# #%%
+proj_attr = (connectivity_reg_ * population_sizes_[:, None]).T
+similarity = proj_attr @ connectivity_reg_
+rate_avg = (S * rates.T @ connectivity_reg_  / np.diagonal(similarity)).T
+#%
+to_save_dict = {"rate_avg": rate_avg, "similarity": similarity}
+# np.save(os.path.join(ACTIVITIES_DIR, f"s{job_env}-jf{int(CONT_FORTH * NUM_NEURONS)}-n{NOISE_STD}-activities"), rate_avg)
+# np.save(os.path.join(SIMILARITIES_DIR, f"s{job_env}-jf{int(CONT_FORTH * NUM_NEURONS)}-n{NOISE_STD}-similarities"), similarity)
+#%
+file_path = os.path.join(
+    RESULTS_DIR, f"s{job_env}-jf{int(CONT_FORTH * NUM_NEURONS)}-n{NOISE_STD}.p"
+)  ############ MEEEEEEEEEEEEEEEEEEEEEE
+pickle.dump(to_save_dict, open(file_path, "wb"))  ######## MEEEEEEEEEEEEEEEEEE
+# np.save(os.path.join(RESULTS_DIR, f"s{job_env}-jf{int(CONT_FORTH * NUM_NEURONS)}-n{NOISE_STD}"), to_save_dict)
+# row, col = np.where(rate_avg > 15)
+#########import seaborn as sns
+#########plt.subplot(211)
+#########to_line_plot = rate_avg.T  # pd.DataFrame(np.rot90(rate_avg[:, :100])) commented for cluster
+#########fig_activities = sns.lineplot(data=to_line_plot, dashes=False, palette="colorblind") #commented for cluster
+########## plt.show() commented for cluster
+#########
+#########plt.subplot(212)
+#########fig_attractors = sns.heatmap(rate_avg) #commented for cluster
+#########plt.show()
+##########%%
